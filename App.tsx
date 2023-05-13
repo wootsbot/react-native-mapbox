5dc152f762a39a57fbda5bf6eb918212a9f34331
J.L.C.A <jorgelca1205@gmail.com>
2023-05-13 13:43:43 -0600

    feat(core): Config init 🚀

---
+++ App.tsx
@@ -0,0 +1,54 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StatusBar, StyleSheet, View} from 'react-native';
+
+import Mapbox from '@rnmapbox/maps';
+
+const MAPBOX_ACCESS_TOKEN =
+  'sk.eyJ1Ijoid29vdHNib3QiLCJhIjoiY2xobTg0aG94MTkxZjNtcW9iamdxdTZmNyJ9.X6QPrEf2_x6NHPxnsHjPlA';
+
+Mapbox.setAccessToken(MAPBOX_ACCESS_TOKEN);
+
+const centerCoord = [-99.4006254, 17.5749345];
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <StatusBar />
+      <View style={styles.page}>
+        <View style={styles.container}>
+          <Mapbox.MapView style={styles.map}>
+            <Mapbox.Camera
+              defaultSettings={{centerCoordinate: centerCoord, zoomLevel: 14}}
+              centerCoordinate={centerCoord}
+              zoomLevel={14}
+            />
+          </Mapbox.MapView>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    height: '100%',
+    width: '100%',
+  },
+  map: {
+    flex: 1,
+  },
+});
+
+export default App;
---
+++ src/libs/mapbox.ts
@@ -0,0 +1,6 @@
+import RNMapbox from '@rnmapbox/maps';
+
+const MAPBOX_ACCESS_TOKEN =
+  'sk.eyJ1Ijoid29vdHNib3QiLCJhIjoiY2xobTg0aG94MTkxZjNtcW9iamdxdTZmNyJ9.X6QPrEf2_x6NHPxnsHjPlA';
+
+export default RNMapbox.setAccessToken(MAPBOX_ACCESS_TOKEN);
